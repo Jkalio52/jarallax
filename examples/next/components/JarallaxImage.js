4f177185eaa83ae7054c830df8077dc4a91e8bb5
Nikita <nkdevinfo@gmail.com>
2026-03-17 10:40:46 +0300

    Update workspace settings and example components

--- examples/next/components/JarallaxImage.js
+++ examples/next/components/JarallaxImage.js
@@ -1,3 +1,3 @@
-export default function JarallaxImage({ className = '', ...props }) {
+export default function JarallaxImage({ className = "", ...props }) {
   return <img className={`jarallax-img ${className}`.trim()} {...props} />;
 }
